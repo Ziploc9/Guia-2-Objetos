f0e39f8fbd08de84569f6cf57a4a3e1b1681afe2
Main <vallejomartin6@gmail.com>
2021-04-09 17:14:33 -0300

    creacion de branch martin

---
+++ Guia-2-Objetos/src/Classes/Autor.java
@@ -0,0 +1,8 @@
+package Classes;
+
+public class Autor {
+    private String nombre;
+    private String apellido;
+    private String email;
+    private char genero;
+}
---
+++ Guia-2-Objetos/src/Classes/Libro.java
@@ -0,0 +1,7 @@
+package Classes;
+
+public class Libro {
+    private String titulo;
+    private int stock;
+    private Autor autor;
+}
